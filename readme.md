a3c52f44ed72c09b4c9aeb92f1f5e678fd5badc7
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-16 08:53:47 +0000

    Release 1.0, see readme.txt for changelog."
    git-svn-id: https://plugins.svn.wordpress.org/yoast-comment-hacks/trunk@1181713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.md
@@ -0,0 +1,33 @@
+[![Code Climate](https://codeclimate.com/github/Yoast/comment-hacks/badges/gpa.svg)](https://codeclimate.com/github/Yoast/comment-hacks)
+
+# Yoast comment hacks
+
+Make comments management easier by applying some of the simple hacks the Yoast team uses.
+
+## Description
+
+This plugin adds some small hacks around core WordPress comments to make them more bearable:
+
+* Cleaner comment notification emails.
+* The option to disallow comments below a certain length.
+* The option to redirect first time commenters to a thank you page.
+* An input field on the comment edit screen to change the comment parent ID.
+* Links in the admin comments section to email individual commenters.
+* A button in the WP toolbar to email all the commenters on a post.
+
+## Screenshots
+
+Screenshot of a clean comment notification email:<br>
+![Screenshot of a clean comment notification email](svn-assets/screenshot-1.png)
+
+The comment parent edit box:<br>
+![The comment parent edit box](svn-assets/screenshot-2.png)
+
+The plugins admin settings:<br>
+![The plugins admin settings](svn-assets/screenshot-3.png)
+
+The button on the frontend to email all the commenters on a post:<br>
+![The button to email all commenters](svn-assets/screenshot-4.png)
+
+The link in the backend to email an individual commenters on a post:<br>
+![The button to email all commenters](svn-assets/screenshot-5.png)
---
+++ yoast-comment-hacks.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package YoastCommentHacks
+
+Plugin Name: Yoast Comment Hacks
+Version: 1.0
+Plugin URI: https://yoast.com/wordpress/plugins/comment-hacks/
+Description: Make comments management easier by applying some of the simple hacks the Yoast team uses.
+Author: Team Yoast
+Author URI: https://yoast.com/
+
+Copyright 2009-2015 Team Yoast (email: support@yoast.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * Used for version checks
+ */
+define( 'YOAST_COMMENT_HACKS_VERSION', '1.0' );
+
+/**
+ * Used for asset embedding
+ */
+define( 'YOAST_COMMENT_HACKS_FILE', __FILE__ );
+
+if ( ! defined( 'YST_COMMENT_HACKS_PATH' ) ) {
+	define( 'YST_COMMENT_HACKS_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+/* ***************************** CLASS AUTOLOADING *************************** */
+if ( file_exists( YST_COMMENT_HACKS_PATH . '/vendor/autoload_52.php' ) ) {
+	require YST_COMMENT_HACKS_PATH . '/vendor/autoload_52.php';
+}
+
+/**
+ * Load translations
+ */
+function yoast_ch_load_textdomain() {
+	load_plugin_textdomain( 'yoast-comment-hacks', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
+add_action( 'init', 'yoast_ch_load_textdomain', 1 );
+
+new YoastCommentHacks();
+
